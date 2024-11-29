6121affcfb37fe43ba28b9c52a8e503193f4421d
nitheesh3000 <131586524+nitheesh3000@users.noreply.github.com>
2024-11-29 15:28:12 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int num = 5;
+    int *ptr = &num;
+    printf("Value of num: %d\n",num);
+    printf("Address of num; %p\n",(void*)&num);
+    printf("value of ptr: %p\n",(void*)ptr);
+    printf("Dereferenced value: %d\n",*ptr);
+    return 0;
+}
